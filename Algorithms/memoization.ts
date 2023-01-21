3fbf8b29a833fbace2ee7e739b77c182080dc065
Kalpesh Bharambe <bharambe.kalpesh@gmail.com>
2023-01-21 22:20:02 +0530

    Update memoization.ts

--- Algorithms/memoization.ts
+++ Algorithms/memoization.ts
@@ -15,3 +15,22 @@ const memoized = memoizedAddTo80();
 
 console.log('1 : ',memoized(5));
 console.log('2 : ',memoized(5));
+
+
+function memoizedFibonacci(){
+  let cache = {};
+  return function fib(n){
+    if(!cache[n]){
+      if(n<2){
+        return n;
+      }else{
+        cache[n]=fib(n-1)+fib(n-2);
+      }
+    }
+    return cache[n]
+  }
+}
+
+const fibonacci = memoizedFibonacci();
+
+console.log(fibonacci(400));
